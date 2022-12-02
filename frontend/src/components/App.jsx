9fcab812ae5201a219e368916b214614ff9d3ed9
Andrey Krymski <6714223@mail.ru>
2022-12-02 10:20:21 +0300

    rollbar examp

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import { Provider as StoreProvider } from 'react-redux';
+// import { Provider, ErrorBoundary } from '@rollbar/react';
 import '../style/App.css';
 import AuthProvider from './mainComponents/AuthProvider.jsx';
 import store from '../slices/index.js';
@@ -7,13 +8,31 @@ import BrowserRouter from './mainComponents/BrowserRouter.jsx';
 import SocketProvider from './mainComponents/SocketProvider';
 import '../i18n.js';
 
+// const rollbarConfig = {
+//  accessToken: 'POST_CLIENT_ITEM_ACCESS_TOKEN',
+//  environment: 'production',
+//  server: {
+//    root: 'http://localhost:3000/',
+//    branch: 'main',
+//  },
+//  code_version: '0.13.7',
+//  person: {
+//    id: 117,
+//    email: 'chief@unsc.gov',
+//    username: 'john-halo',
+//  },
+// };
 const App = () => (
-  <Provider store={store}>
+// <Provider config={rollbarConfig}>
+  //  <ErrorBoundary>
+  <StoreProvider store={store}>
     <SocketProvider>
       <AuthProvider>
         <BrowserRouter />
       </AuthProvider>
     </SocketProvider>
-  </Provider>
+  </StoreProvider>
+  //  </ErrorBoundary>
+//  </Provider>
 );
 export default App;
